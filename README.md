6a83a88ffc2396ef9a2f0f4d288ed4b9e39399c7
Juan Antonio Castañeda Ramirez <castaneda2850@gmail.com>
2024-10-27 20:53:31 -0500

    readme.md

---
+++ README.md
@@ -0,0 +1,18 @@
+# API PRODUCTO
+## API CRUD de productos con Java JDK 21 Sprign Boot 3.3.4, crear uan Base de datos nombre dbpharmanet
+
+### Listar productos
+![Texto alternativo](src/main/resources/static/img/ImageCinco.png)
+### Eliminar un producto que no existe por id 
+![Texto alternativo](src/main/resources/static/img/ImageDos.png)
+### Eliminar un producto existente por id
+![Texto alternativo](src/main/resources/static/img/ImageTres.png)
+### Agregar producto
+![Texto alternativo](src/main/resources/static/img/ImagenCuatro.png)
+### En campos nulos o blancos
+![Texto alternativo](src/main/resources/static/img/Imagen.png)
+### Actualizar un producto de existente
+![Texto alternativo](src/main/resources/static/img/imagenUno.png)
+
+
+
--- src/main/java/com/pharmanet/pharmanet/config/AppConfig.java
+++ src/main/java/com/pharmanet/pharmanet/config/AppConfig.java
@@ -3,11 +3,33 @@ package com.pharmanet.pharmanet.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173") // Cambia por la URL de tu frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
